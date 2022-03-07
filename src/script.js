2ae6d32149148a885386b99cff690867bb17426c
ale <ale@ale.com>
2022-03-07 10:32:07 -0300

    feat: feature navegation to users

--- src/script.js
+++ src/script.js
@@ -1,14 +1,12 @@
-// import { headerMobileVerion } from "./functions/app_Header.js";
-// import { headerDesktopVersion } from "./functions/app_Header.js";
+import { searchUser } from "./functions/app_Header.js";
 
 const started_page = document.getElementById('started_page');
-
-const body = document.querySelector('body');
-const proprietes_element = body.getClientRects().item(body)
-const size_screen = proprietes_element.width
+const btn_search = document.getElementById('button_research');
 
 started_page.addEventListener('click', () => {
     const page = document.getElementById('frontPage')
     page.classList.remove('flip');
 
 });
+
+btn_search.addEventListener('click', searchUser)
